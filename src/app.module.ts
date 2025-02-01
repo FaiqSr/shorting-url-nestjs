d6493ce666114e0847bb16cd52ba405c633675d0
FaiqSr <FaiqSr@github.com>
2025-02-01 17:07:50 +0700

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
 import { UrlModule } from './url/url.module';
+import { ShortUrlModule } from './shortUrl/shortUrl.module';
 
 @Module({
-  imports: [UserModule, CommonModule, UrlModule],
+  imports: [UserModule, CommonModule, UrlModule, ShortUrlModule],
   controllers: [],
   providers: [],
 })
